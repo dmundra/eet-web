d22ea16b537a459ae63d175538e98c4c1428d1b7
easteurotopo <easteurotopo@riousa.com>
2020-08-01 15:11:22 -0500

    Added new geojson layers and updated harvard map.

--- harvard-maps.js
+++ harvard-maps.js
@@ -23,10 +23,6 @@ $(document).ready(function() {
     attribution: 'Historical map tiles by ???'
   }).addTo(map);
 
-  southwestrussia_baseoverlay = L.tileLayer('layerdata/southwestrussia/{z}/{x}/{-y}.png', {
-    attribution: 'Historical map tiles by <a href="https://www.davidrumsey.com/luna/servlet/detail/RUMSEY~8~1~37973~1210986:South-West-Russia-?sort=Pub_List_No_InitialSort%2CPub_Date%2CPub_List_No%2CSeries_No&qvq=q:south%20west%20russia%20a.k.%20Johnston;sort:Pub_List_No_InitialSort%2CPub_Date%2CPub_List_No%2CSeries_No;lc:RUMSEY~8~1&mi=3&trs=4">David Rumsey collection</a>'
-  });
-
   var style1 = {
     "color": "#F81F05",
     "weight": 3,
@@ -60,25 +56,32 @@ $(document).ready(function() {
     "weight": 3,
   };
 
-  var geojsonLayer1 = new L.GeoJSON.AJAX("geojson/pale_of_settlement_1804_8wz.geojson", { style: style1 });
-  var geojsonLayer2 = new L.GeoJSON.AJAX("geojson/pale_of_settlement_1809_1815_bul.geojson", { style: style1 });
-  var geojsonLayer3 = new L.GeoJSON.AJAX("geojson/zone_of_jewish_residence_1793_k3z.geojson", { style: style6 });
-  var geojsonLayer4 = new L.GeoJSON.AJAX("geojson/the_emerging_pale_of_settlement_1800_zkl.geojson", { style: style6 });
-  var geojsonLayer5 = new L.GeoJSON.AJAX("geojson/pale_of_settlement_1807_4ib.geojson", { style: style1 });
-  var geojsonLayer6 = new L.GeoJSON.AJAX("geojson/zone_of_jewish_residence_1795_6jg.geojson", { style: style6 });
-  var geojsonLayer7 = new L.GeoJSON.AJAX("geojson/first_russian_partition_1772_3fj.geojson", { style: style2 });
-  var geojsonLayer8 = new L.GeoJSON.AJAX("geojson/second_russian_partition_1793_g5j.geojson", { style: style2 });
-  var geojsonLayer9 = new L.GeoJSON.AJAX("geojson/third_russian_partition_1795_ouu.geojson", { style: style2 });
-  var geojsonLayer10 = new L.GeoJSON.AJAX("geojson/novorossiya_1792_jxw.geojson", { style: style5 });
-  var geojsonLayer11 = new L.GeoJSON.AJAX("geojson/bialystock_oblast_1807_1842_2db.geojson", { style: style5 });
-  var geojsonLayer12 = new L.GeoJSON.AJAX("geojson/bessarabia_k12.geojson", { style: style5 });
-  var geojsonLayer13 = new L.GeoJSON.AJAX("geojson/tarnopol_addition_1809_1815_23v.geojson", { style: style5 });
-  var geojsonLayer14 = new L.GeoJSON.AJAX("geojson/zone_of_jewish_residence_1791_wqx.geojson", { style: style6 });
-  var geojsonLayer15 = new L.GeoJSON.AJAX("geojson/polish_lithuanian_commonwealth_1667_1772.geojson", { style: style7 });
-  var geojsonLayer16 = new L.GeoJSON.AJAX("geojson/county_administrative_centers_i0n.json", { style: style8 });
-  var geojsonLayer17 = new L.GeoJSON.AJAX("geojson/pale_of_settlement_1815_1818_fu4.json", { style: style1 });
-  var geojsonLayer18 = new L.GeoJSON.AJAX("geojson/pale_of_settlement_1818_myx.json", { style: style1 }).addTo(map);
-  var geojsonLayer19 = new L.GeoJSON.AJAX("geojson/congress_poland_1815_0pc.json", { style: style3 }).addTo(map);
+  // Village explusions.
+  var geojsonLayer1 = new L.GeoJSON.AJAX("geojson/harvard/grodno_gubernia_nwt.geojson", { style: style3 });
+  var geojsonLayer2 = new L.GeoJSON.AJAX("geojson/harvard/vitbesk_mogllev_village_expulsion_1823_0yb.geojson", { style: style3 });
+  var geojsonLayer3 = new L.GeoJSON.AJAX("geojson/harvard/chernigov_village_expulsion_1825_6nw.geojson", { style: style3 });
+  // The emerging Pale of Settlement.
+  var geojsonLayer4 = new L.GeoJSON.AJAX("geojson/harvard/pale_of_settlement_1818_a5r.geojson", { style: style6 });
+  var geojsonLayer5 = new L.GeoJSON.AJAX("geojson/harvard/pale_of_settlement_1809_1815_jpa.geojson", { style: style1 }).addTo(map);
+  var geojsonLayer6 = new L.GeoJSON.AJAX("geojson/harvard/pale_of_settlement_1807_51a.geojson", { style: style6 });
+  var geojsonLayer7 = new L.GeoJSON.AJAX("geojson/harvard/pale_of_settlement_1804_y3v.geojson", { style: style6 });
+  var geojsonLayer8 = new L.GeoJSON.AJAX("geojson/harvard/emerging_pale_1800_lxe.geojson", { style: style6 });
+  var geojsonLayer9 = new L.GeoJSON.AJAX("geojson/harvard/jewish_residence_after_the_second_partit_g3a.geojson", { style: style6 });
+  var geojsonLayer10 = new L.GeoJSON.AJAX("geojson/harvard/jewish_residence_1791_hn1.geojson", { style: style6 });
+  // Non-partition acquisitions
+  var geojsonLayer11 = new L.GeoJSON.AJAX("geojson/harvard/bessarabia_k0j.geojson", { style: style5 });
+  var geojsonLayer12 = new L.GeoJSON.AJAX("geojson/harvard/congress_poland_1815_l09.geojson", { style: style5 });
+  var geojsonLayer13 = new L.GeoJSON.AJAX("geojson/harvard/tarnopol_addition_9va.geojson", { style: style5 });
+  var geojsonLayer14 = new L.GeoJSON.AJAX("geojson/harvard/bialystock_oblast_to2.geojson", { style: style5 });
+  var geojsonLayer15 = new L.GeoJSON.AJAX("geojson/harvard/novorossiya_1792_6no.geojson", { style: style5 });
+  // Partitions of Poland-Lithuania
+  var geojsonLayer16 = new L.GeoJSON.AJAX("geojson/harvard/first_austrian_partition_1772_hso.geojson", { style: style3 });
+  var geojsonLayer17 = new L.GeoJSON.AJAX("geojson/harvard/first_austrian_partition_1772_9np.geojson", { style: style3 });
+  var geojsonLayer18 = new L.GeoJSON.AJAX("geojson/harvard/third_russian_partition_of_poland_lithua_dfa.geojson", { style: style2 });
+  var geojsonLayer19 = new L.GeoJSON.AJAX("geojson/harvard/second_russian_partition_of_poland_lithu_eze.geojson", { style: style2 });
+  var geojsonLayer20 = new L.GeoJSON.AJAX("geojson/harvard/first_russian_partition_of_poland_lithua_duv.geojson", { style: style2 });
+  // Commonwealth of Polish-Lithuanian
+  var geojsonLayer21 = new L.GeoJSON.AJAX("geojson/harvard/polish_lithuanian_commonwealth_1667_1772_428.geojson", { style: style7 });
 
   var baseMaps = {
     "Stamen Toner": stamen_toner,
@@ -88,44 +91,44 @@ $(document).ready(function() {
 
   var overlayMaps = {
     "General": {
-      "South-West Russia 1893": southwestrussia_baseoverlay,
       "Harvard Overlay": harvard_baseoverlay
     },
-    "Polish-Lithuanian Commonwealth": {
-      "Polish-Lithuanian Commonwealth<br/>1667-1772": geojsonLayer15,
+    "Commonwealth of Polish-Lithuanian": {
+      "Polish-Lithuanian 1667-1772": geojsonLayer21,
     },
     "Partitions of Poland-Lithuania": {
-      "First Russian Partition 1772": geojsonLayer7,
-      "Second Russian partition 1793": geojsonLayer8,
-      "Third Russian partition 1795": geojsonLayer9,
+      "1st. Russian partition 1772": geojsonLayer20,
+      "2nd. Russian partition 1793": geojsonLayer19,
+      "3rd Russian partition 1795": geojsonLayer18,
+      "1st. Austrian partition 1772": geojsonLayer17,
+      "Austrian Galicia 1809": geojsonLayer16,
     },
-    "Pre and Post Partition Acquisitions": {
-      "Novorossiya 1792": geojsonLayer10,
-      "Bialystock oblast 1807-1842": geojsonLayer11,
-      "Bessarabia oblast 1812": geojsonLayer12,
+    "Non-partition acquisitions": {
+      "Novorossiya 1792": geojsonLayer15,
+      "Bialystock oblast 1807": geojsonLayer14,
       "Tarnopol Addition 1809-1815": geojsonLayer13,
-      "Congress Poland 1815": geojsonLayer19,
+      "Congress Poland 1815": geojsonLayer12,
+      "Bessarabia": geojsonLayer11,
     },
-    "The Emerging Pale of Settlement": {
-      "Zone of Jewish residence 1791": geojsonLayer14,
-      "Zone of Jewish residence 1793": geojsonLayer3,
-      "Zone of Jewish residence 1795": geojsonLayer6,
-      "The emerging Pale 1800": geojsonLayer4,
-      "Pale of Settlement 1804": geojsonLayer1,
-      "Pale of Settlement 1807": geojsonLayer5,
-      "Pale of Settlement 1809-1815": geojsonLayer2,
-      "Pale of Settlement 1815-1818": geojsonLayer17,
-      "Pale of Settlement 1818": geojsonLayer18,
+    "The emerging Pale of Settlement": {
+      "Jews in Russia 1791": geojsonLayer10,
+      "Jewish residence after the 2nd partition 1794": geojsonLayer9,
+      "Emerging Pale 1800": geojsonLayer8,
+      "Pale of Settlement 1804": geojsonLayer7,
+      "Pale of Settlement 1807": geojsonLayer6,
+      "Pale of Settlement 1809-1815": geojsonLayer5,
+      "Pale of Settlement 1818": geojsonLayer4,
     },
-    "Towns of the Pale of Settlement": {
-      "County Administrative Centers": geojsonLayer16,	
+    "Village explusions": {
+      "Chernigov gubernia 1821": geojsonLayer3,
+      "Vitbesk-Mogllev gubernii 1823": geojsonLayer2,
+      "Grodno gubernia 1827": geojsonLayer1,
     },
   };
 
   L.control.groupedLayers(baseMaps, overlayMaps, {collapsed:false}).addTo(map);
 
   // Really hacky opacity slider.
-  $("span:contains('South-West Russia 1893')").html(' South-West Russia 1893<br/><input id="slide" type="range" min="0" max="1" step="0.1" value="1.0" oninput="southwestrussia_baseoverlay.setOpacity(this.value)">');
   $("span:contains('Harvard Overlay')").html(' Harvard Overlay<br/><input id="slide" type="range" min="0" max="1" step="0.1" value="1.0" oninput="harvard_baseoverlay.setOpacity(this.value)">');
 
   $('.leaflet-control-layers').hide();
